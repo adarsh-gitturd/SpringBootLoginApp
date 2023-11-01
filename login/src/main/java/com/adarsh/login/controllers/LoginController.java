a110c9748bc4f24da5b391674a11bb2c8937da75
Adarsh Wilson <adarshwilsona@gmail.com>
2023-11-01 22:59:20 +0530

    annoying retarded bugs

---
+++ login/src/main/java/com/adarsh/login/controllers/LoginController.java
@@ -0,0 +1,20 @@
+package com.adarsh.login.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class LoginController {
+
+    @RequestMapping("hello")
+    @ResponseBody
+    public String hello(){
+        return "HELLO HELLO";
+    }
+
+    @RequestMapping("login")
+    public String lo(){
+        return "logina";
+    }
+}
---
+++ login/src/main/resources/META-INF/resources/WEB-INF/jsp/logina.jsp
@@ -0,0 +1,9 @@
+<html>
+<head>
+    <title> Login Page </title>
+</head>
+
+<body>
+    HI HELLO JSP
+</body>
+</html>
